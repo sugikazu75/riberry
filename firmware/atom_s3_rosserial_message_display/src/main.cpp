dfbc7637830aa709187ad167275890bd6f4bead8
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2024-10-09 11:41:34 +0900

    [rosserial] calc voltage by aerial robot system

--- firmware/atom_s3_rosserial_message_display/src/main.cpp
+++ firmware/atom_s3_rosserial_message_display/src/main.cpp
@@ -18,17 +18,30 @@
 class BatteryDisplay
 {
 public:
-  BatteryDisplay(float maxVoltage, float minVoltage);
+  BatteryDisplay(float maxVoltage, float minVoltage, int bat_cell);
 
 public:
   void setVoltageRange(float newMaxVoltage, float newMinVoltage);
   void displayFrame();
   void updateVoltage(float voltage);
+  float calcPercentage(float voltage);
+  static constexpr float VOLTAGE_100P =  4.2;
+  static constexpr float VOLTAGE_90P =  4.085;
+  static constexpr float VOLTAGE_80P =  3.999;
+  static constexpr float VOLTAGE_70P =  3.936;
+  static constexpr float VOLTAGE_60P =  3.883;
+  static constexpr float VOLTAGE_50P =  3.839;
+  static constexpr float VOLTAGE_40P =  3.812;
+  static constexpr float VOLTAGE_30P =  3.791;
+  static constexpr float VOLTAGE_20P =  3.747;
+  static constexpr float VOLTAGE_10P =  3.1;
+  static constexpr float VOLTAGE_0P =  3.0;
 
 private:
   float maxVoltage_;
   float minVoltage_;
   float Voltage_;
+  int bat_cell_;
 };
 
 #endif
@@ -37,8 +50,8 @@ private:
 #define LCD_W M5.Lcd.width()
 
 
-BatteryDisplay::BatteryDisplay(float maxVoltage, float minVoltage)
-  :maxVoltage_(maxVoltage), minVoltage_(minVoltage) {}
+BatteryDisplay::BatteryDisplay(float maxVoltage, float minVoltage, int bat_cell)
+  :maxVoltage_(maxVoltage), minVoltage_(minVoltage), bat_cell_(bat_cell) {}
 
 void BatteryDisplay::setVoltageRange(float newMaxVoltage, float newMinVoltage)
 {
@@ -61,9 +74,26 @@ void BatteryDisplay::displayFrame()
   M5.Lcd.drawString("%", LCD_W-12, 22, 1);
 }
 
+float BatteryDisplay::calcPercentage(float voltage){
+  float average_voltage = voltage / bat_cell_;
+  float input_cell = voltage / VOLTAGE_100P;
+  float percentage = 0;
+  if(average_voltage  > VOLTAGE_90P) percentage = (average_voltage - VOLTAGE_90P) / (VOLTAGE_100P - VOLTAGE_90P) * 10 + 90;
+  else if (average_voltage  > VOLTAGE_80P) percentage = (average_voltage - VOLTAGE_80P) / (VOLTAGE_90P - VOLTAGE_80P) * 10 + 80;
+  else if (average_voltage  > VOLTAGE_70P) percentage = (average_voltage - VOLTAGE_70P) / (VOLTAGE_80P - VOLTAGE_70P) * 10 + 70;
+  else if (average_voltage  > VOLTAGE_60P) percentage = (average_voltage - VOLTAGE_60P) / (VOLTAGE_70P - VOLTAGE_60P) * 10 + 60;
+  else if (average_voltage  > VOLTAGE_50P) percentage = (average_voltage - VOLTAGE_50P) / (VOLTAGE_60P - VOLTAGE_50P) * 10 + 50;
+  else if (average_voltage  > VOLTAGE_40P) percentage = (average_voltage - VOLTAGE_40P) / (VOLTAGE_50P - VOLTAGE_40P) * 10 + 40;
+  else if (average_voltage  > VOLTAGE_30P) percentage = (average_voltage - VOLTAGE_30P) / (VOLTAGE_40P - VOLTAGE_30P) * 10 + 30;
+  else if (average_voltage  > VOLTAGE_20P) percentage = (average_voltage - VOLTAGE_20P) / (VOLTAGE_30P - VOLTAGE_20P) * 10 + 20;
+  else if (average_voltage  > VOLTAGE_10P) percentage = (average_voltage - VOLTAGE_10P) / (VOLTAGE_20P - VOLTAGE_10P) * 10 + 10;
+  else percentage = (average_voltage - VOLTAGE_0P) / (VOLTAGE_10P - VOLTAGE_0P) * 10;
+  return percentage;
+}
+
 void BatteryDisplay::updateVoltage(float voltage)
 {
-  float voltageRatio = (voltage - minVoltage_) / (maxVoltage_ - minVoltage_);
+  float voltageRatio = calcPercentage(voltage);
   voltageRatio = constrain(voltageRatio, 0.0f, 1.0f);
 
   // Erase screen.
@@ -109,7 +139,8 @@ ros::Subscriber<std_msgs::Float32> battery_voltage_sub_("battery_voltage_status"
 
 float maxVoltage = 16.8f; // 100% voltage
 float minVoltage = 14.8f; // 0%   voltage
-BatteryDisplay batDisp(maxVoltage, minVoltage);
+int batcell = 4;
+BatteryDisplay batDisp(maxVoltage, minVoltage, batcell);
 
 void setup()
 {
